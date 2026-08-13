efa52766607ae16a2a0228f156e2f92e35314b5e
ShahriarSajib <sajibhossen516@gmail.com>
2026-08-13 12:21:33 +0600

    connected database

--- server/src/utils/AppError.js
+++ server/src/utils/AppError.js
@@ -1,5 +1,14 @@
 /**
  * utils/AppError.js — typed error with HTTP status + code.
  */
-// TODO
-module.exports = {};
+class AppError extends Error {
+  constructor(message, statusCode = 500, code = "INTERNAL_ERROR") {
+    super(message);
+    this.statusCode = statusCode;
+    this.code = code;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
